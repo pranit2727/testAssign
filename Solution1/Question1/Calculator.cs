6c954f53eefaf544135debb2136b896742e1bbc8
pranit2727 <45756122+pranit2727@users.noreply.github.com>
2018-12-14 17:53:41 +0530

    Add files via upload

---
+++ Solution1/Question1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Question1
+{
+    public class Calculator
+    {
+        //Calculating Sum of two Numbers
+        public virtual int Calculate(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+    }
+    public class ChildClass : Calculator
+    {
+        //Overiding the base implementation and Calculating multiplication of two Numbers
+        public override int Calculate(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+    
+}
+
+
---
+++ Solution1/Question1/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Question1
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+        //Constructor of Person class
+        public Person()
+        {
+            Name = "Rohit";
+            Age = 10;
+        }
+        
+    }
+    
+    public class Employee : Person
+    {
+        public string name
+        {
+            get { return this.Name; }
+            set { this.Name = value; }
+        }
+
+        public int age
+        {
+            get { return this.Age; }
+            set { this.Age = value; }
+        }
+
+   
+        //printing the value of person
+        public void Print()
+        {
+            Console.WriteLine("The name of person is {0} and age is {1} \n", name, age);
+        }
+
+
+    }
+
+
+    public class mainMethod
+    {
+        public static void Main(string[] args)
+        {
+            //Setting the value of person through employee class
+            Employee emp = new Employee();
+            emp.name = "abhi";
+            emp.age = 24;
+            emp.Print();
+
+            //Object of calculator class is created and invoking base class's method
+            Calculator baseClassObj;
+            baseClassObj = new Calculator();
+            Console.WriteLine("Base class method Calculate and sum is  :" + baseClassObj.Calculate(3, 8));
+
+            //Object of childClass is created and invoking derived class's method
+            baseClassObj = new ChildClass();
+            Console.WriteLine("Child class method Calculate and multiplication is:" + baseClassObj.Calculate(3, 8));
+            Console.ReadKey();
+        }
+
+    }
+}
+
